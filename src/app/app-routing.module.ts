5b84a65da59ff76fa819aa0ba2f954f86b8407ab
Renato <renatoescorciac@gmail.com>
2023-07-24 23:10:10 -0500

    24 de julio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginGuard } from './guards/login.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'menu/home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
